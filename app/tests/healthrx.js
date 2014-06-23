58bb02ff7c45394f43ccdc544842c1e18f16ce14
Robby <robby.fairclough@gmail.com>
2014-06-22 18:03:15 -0700

    css tweaks

--- app/tests/healthrx.js
+++ app/tests/healthrx.js
@@ -6,8 +6,40 @@ var system = require( 'system' );
 var s4 = system.args[ 1 ];
 console.log( s4 );
 
-page.open( 'https://orderhealthrx.com/HEALTHRX_004_MAS_CON_DUAL_1841_1843_ST8UP_05202014/', function() {
-  setTimeout( function() {
-    phantom.exit();
-  }, 5000 )
-});
+page.open( 'https://orderhealthrx.com/HEALTHRX_004_MAS_CON_DUAL_1841_1843_ST8UP_05202014/' );
+
+page.onConsoleMessage = function( msg ) {
+  console.log( msg );
+};
+
+page.onLoadFinished = function( status ) {
+  if( 'success' !== status ) phantom.exit();
+
+  page.includeJs( 'https://cdnjs.cloudflare.com/ajax/libs/mocha/1.20.1/mocha.js', function() {
+    page.includeJs( 'https://cdnjs.cloudflare.com/ajax/libs/chai/1.9.1/chai.js', function() {
+      var res = page.evaluate( function() {
+        console.log( 'mocha:', mocha );
+        console.log( 'chai:', chai );
+
+        mocha.ui( 'bdd' );
+        mocha.reporter( 'json' );
+
+        var expect = chai.expect;
+
+        describe( 'DOM tests', function() {
+          var form = document.querySelector( '#opt_in_form' );
+
+          it( 'is in the DOM', function() {
+            expect( form ).to.not.equal( null );
+          });
+        });
+
+        return mocha.run();
+      });
+
+      console.log( res );
+
+      phantom.exit();
+    });
+  });
+};
